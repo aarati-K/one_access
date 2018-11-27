73db2b93bfc62450401045a4a229addce31f92c8
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2018-11-26 18:27:38 -0600

    Update load.py

--- load/load.py
+++ load/load.py
@@ -17,11 +17,11 @@ class DataLoader():
     # Start separate processes for sample_creator(s)
     sc = SampleCreator(self.data_store, 0)
     sc.start()
-    sc.join()
     
     # Start batch_creator(s)
     
     # based on the memory config
+    
 
   def get_next_batch(self):
     """
