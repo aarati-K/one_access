b654cd8f2885a233b85d9665c2e433ca84e00fa1
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2018-11-26 20:27:43 -0600

    Rename load, store, and perform reservoir sampling on points

--- sampling/sample_creator.py
+++ sampling/sample_creator.py
@@ -13,7 +13,7 @@ class SampleCreator(Process):
   def __init__(self, data_store):
     super(P, self).__init__()
     self.ds = data_store
-    self.md = self.data_store.metadata
+    self.num_points = self.ds.num_points
 
   def run(self):
     """
@@ -23,22 +23,27 @@ class SampleCreator(Process):
       reservoir = Sample(self.ds.sample_size)
 
       i = 0
-      while i < reservoir.maxsize and i < self.md.num_points:
+      while i < reservoir.maxsize and i < self.num_points:
         point = self.get_next_point()
         reservoir.items.append(point)
         i += 1
         
-      while i >= reservoir.maxsize and i < self.md.num_points:
-        point = self.get_next_point()
+      point = reservoir.maxsize
+      points_to_fill = []
+      while i >= reservoir.maxsize and i < self.num_points:
+        point += 1
         s = random.randint(0, reservoir.maxsize + i)
         if s < m:
-          reservoir.items[s] = point
+          points_to_fill.append((m, point))
         i += 1
 
       # Blocks if max number of samples met
+      fill_reservoir_with_points(reservoir, points_to_fill)
+
+      # TODO: Only for memory sampling rn, add SSD/Disk support later
       self.ds.samples.put(reservoir)
     
-  def get_next_point(self):
+  def fill_reservoir_with_points(self, reservoir, points):
     """ Get next blob into memory, read points from it, and then move on to
       next blob from ir location. """
     # TODO:
--- store/metadata.py
+++
@@ -1,6 +0,0 @@
-class Metadata:
-    """ Metadata class is associated with each datastore. """
-    def __init__(self, md_filepath):
-        # TODO:
-        self.num_points = 0
-        self.ir_loc = ""
