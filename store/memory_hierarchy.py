b952543cc8720f991f06b170cc8575c9ce18ba35
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2018-12-06 12:57:36 -0600

    PEP8 style applied to memory_hierarchy.py

--- store/memory_hierarchy.py
+++ store/memory_hierarchy.py
@@ -1,31 +1,34 @@
 import json
 import os
 
+
 class StorageComponents():
-  SSD = 'ssd'
-  HDD = 'hdd'
+    SSD = 'ssd'
+    HDD = 'hdd'
+
 
 class StorageAttributes():
-  MOUNT_POINT = "mount_point"
-  ONE_ACCESS_DIR = "one_access_dir"
+    MOUNT_POINT = "mount_point"
+    ONE_ACCESS_DIR = "one_access_dir"
+
 
 class MemoryHierarchy():
-  """
+    """
     Read the memory hierarchy specified in memory.config. By
     default, we assume that as much main_memory can be used as
     necessary/available. 
   """
-  mem_config = {}
+    mem_config = {}
 
-  @classmethod
-  def load(cls):
-    cur_dir = os.path.dirname(os.path.realpath(__file__))
-    config_file = os.path.join(cur_dir, os.pardir, "memory.config")
-    data = open(config_file).read()
-    cls.mem_config = json.loads(data)
+    @classmethod
+    def load(cls):
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        config_file = os.path.join(cur_dir, os.pardir, "memory.config")
+        data = open(config_file).read()
+        cls.mem_config = json.loads(data)
 
-  @classmethod
-  def get_config(cls, component):
-    if component in cls.mem_config.keys():
-      return cls.mem_config[component]
-    return None
+    @classmethod
+    def get_config(cls, component):
+        if component in cls.mem_config.keys():
+            return cls.mem_config[component]
+        return None
