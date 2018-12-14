268d70a58f07a0ff963dc7d8cd34acfb0c33f092
aarati-K <aarati.agc10@gmail.com>
2018-12-14 12:19:58 -0600

    Update plotting code + Include plot

--- benchmarks/cifar10/benchmark_queue_get_put.py
+++ benchmarks/cifar10/benchmark_queue_get_put.py
@@ -30,11 +30,26 @@ print(end-start)
 # 1600: 0.00673
 # 1800: 0.00947
 # 2000: 0.01038
-# Plotting code - Get
-# x = [1, 10, 100, 300, 500, 700, 900, 1000, 1200, 1400, 1600, 1800, 2000]
-# y = [0.000275, 0.000269, 0.00088, 0.00177, 0.00267, 0.00374, 0.00489, 0.00506, 0.00608, 0.00711, 0.00673, 0.00947, 0.01038]
-# plt.plot(x, [2*v for v in y])
+# Plotting code - Get + Put
+# x = [1, 100, 300, 500, 700, 900, 1000, 1200, 1400, 1600, 1800, 2000]
+# y = [0.000275, 0.00088, 0.00177, 0.00267, 0.00374, 0.00489, 0.00506, 0.00608, 0.00711, 0.00673, 0.00947, 0.01038]
+# y = [2*v for v in y]
+# plt.plot(x, y, color='b', marker='o', markerfacecolor='k', markersize=10, fillstyle='full', linewidth=3, linestyle='solid')
+# plt.xlabel("Reservoir Sample Size", fontsize=20, fontweight='semibold', fontname='serif')
+# plt.ylabel("Queue put and get (s)", fontsize=20, fontweight='semibold', fontname='serif')
+# plt.xticks(x, [1, '', '', 500, '', '', 1000, '', '', 1600, '', 2000])
+# _, ticks = plt.xticks()
+# for tick in ticks:
+#     tick.set_fontsize(16)
+#     tick.set_fontweight('medium')
+#     tick.set_fontname('serif')
+# _, ticks = plt.yticks()
+# for tick in ticks:
+#     tick.set_fontsize(16)
+#     tick.set_fontweight('medium')
+#     tick.set_fontname('serif')
 # plt.show()
+
 
 # batch_size = 10
 # rel_sample_size = 1
