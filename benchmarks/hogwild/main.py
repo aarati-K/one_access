8dd79d6510be40d8204aa76bd9462d6c2a2972c2
aarati-K <aarati.agc10@gmail.com>
2018-12-16 21:40:02 -0600

    Common benchmark file for one_access and pytorch

--- benchmarks/hogwild/main.py
+++ benchmarks/hogwild/main.py
@@ -6,20 +6,29 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.multiprocessing as mp
+from torchvision import transforms
 
-from train import train, test
+from load.load import DataLoader
+from store.cifar10 import Cifar10
+from train import train_pytorch, train_one_access, test
+import time
+
+ONE_ACCESS = 1
+PYTORCH = 2
 
 # Training settings
 parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                    help='input batch size for training (default: 64)')
+parser.add_argument('--batch-size', type=int, default=32, metavar='N',
+                    help='input batch size for training (default: 32)')
+parser.add_argument('--rel-sample-size', type=int, default=500, metavar='N',
+                    help="relative sample size for sample creator (default: 500)")
 parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
                     help='input batch size for testing (default: 1000)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
+parser.add_argument('--epochs', type=int, default=1, metavar='N',
                     help='number of epochs to train (default: 20)')
 parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                    help='learning rate (default: 0.001)')
-parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
                     help='SGD momentum (default: 0.5)')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
@@ -27,6 +36,8 @@ parser.add_argument('--log-interval', type=int, default=10, metavar='N',
                     help='how many batches to wait before logging training status')
 parser.add_argument('--num-processes', type=int, default=2, metavar='N',
                     help='how many training processes to use (default: 2)')
+parser.add_argument('--loader', type=int, default=ONE_ACCESS,
+                    help='Which data loader to use')
 
 
 class Net(nn.Module):
@@ -57,14 +68,37 @@ if __name__ == '__main__':
     model = Net()
     model.share_memory() # gradients are allocated lazily, so they are not shared here
 
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    start = time.time()
+    if args.loader == ONE_ACCESS:
+        data_store = Cifar10(
+            input_data_folder="~/datasets", \
+            max_batches=4, batch_size=args.batch_size, \
+            rel_sample_size=args.rel_sample_size, max_samples=1, \
+            transform=transform)
+        data_store.initialize()
+        data_loader = DataLoader(data_store, epochs=(args.epochs*args.num_processes))
+
     processes = []
     for rank in range(args.num_processes):
-        p = mp.Process(target=train, args=(rank, args, model))
-        # We first train the model across `num_processes` processes
+        if args.loader == ONE_ACCESS:
+            p = mp.Process(target=train_one_access, args=(args, model, data_loader))
+            # train_one_access(args, model, data_loader)
+        else:
+            p = mp.Process(target=train_pytorch, args=(rank, args, model))
+            # train_pytorch(rank, args, model)
         p.start()
         processes.append(p)
+
     for p in processes:
         p.join()
+
+    end = time.time()
+    print(end-start)
+    if args.loader == ONE_ACCESS:
+        data_loader.stop_batch_creation()
 
     # Once training is complete, we can test the model
     test(args, model)
--- benchmarks/hogwild/train.py
+++ benchmarks/hogwild/train.py
@@ -6,22 +6,66 @@ import torch.optim as optim
 import torch.nn.functional as F
 import torch.nn as nn
 from torchvision import datasets, transforms
+from cifar import CIFAR10
 
 
-def train(rank, args, model):
+def train_one_access(args, model, data_loader):
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    model.train()
+    pid = os.getpid()
+    i = 0
+    while True:
+        try:
+            data, target = data_loader.get_next_batch()
+        except:
+            break
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        i += 1
+        if i % args.log_interval == 0:
+            print('{}\t{}\tLoss: {:.6f}'.format(
+                pid, i, loss.item()))
+
+
+# def backward_step(loss, optimizer):
+#     loss.backward()
+#     optimizer.step()
+
+
+def train_pytorch(rank, args, model):
     torch.manual_seed(args.seed + rank)
 
     transform = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
     train_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10('~/datasets', train=True, download=True, transform=transform),
+        CIFAR10('~/datasets', train=True, download=True, transform=transform),
         batch_size=args.batch_size, shuffle=True, num_workers=1)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
     for epoch in range(1, args.epochs + 1):
-        train_epoch(epoch, args, model, train_loader, optimizer, criterion)
+        train_epoch_pytorch(epoch, args, model, train_loader, optimizer, criterion)
+
+
+def train_epoch_pytorch(epoch, args, model, data_loader, optimizer, criterion):
+    model.train()
+    pid = os.getpid()
+    i = 0
+    for batch_idx, (data, target) in enumerate(data_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('{}\tTrain Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                pid, epoch, batch_idx * len(data), len(data_loader.dataset),
+                100. * batch_idx / len(data_loader), loss.item()))
 
 
 def test(args, model):
@@ -35,21 +79,6 @@ def test(args, model):
         batch_size=args.batch_size, shuffle=True, num_workers=1)
 
     test_epoch(model, test_loader, criterion)
-
-
-def train_epoch(epoch, args, model, data_loader, optimizer, criterion):
-    model.train()
-    pid = os.getpid()
-    for batch_idx, (data, target) in enumerate(data_loader):
-        optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('{}\tTrain Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                pid, epoch, batch_idx * len(data), len(data_loader.dataset),
-                100. * batch_idx / len(data_loader), loss.item()))
 
 
 def test_epoch(model, data_loader, criterion):
