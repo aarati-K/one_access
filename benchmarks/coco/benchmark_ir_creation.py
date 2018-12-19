2f938484e08bac6d0fe8017fefa1e5de6c376138
aarati-K <aarati.agc10@gmail.com>
2018-12-18 22:23:16 -0600

    Small change in coco benchmark

--- benchmarks/coco/benchmark_ir_creation.py
+++ benchmarks/coco/benchmark_ir_creation.py
@@ -6,6 +6,7 @@ start = time.time()
 ds = CocoDetection(input_data_folder="/home/aarati/datasets/coco/train2017",
                    annFile="/home/aarati/datasets/coco/annotations/instances_train2017.json")
 ds.count_num_points()
+print(ds.num_train_points)
 ds.generate_IR()
 end = time.time()
-print(end-start)
+print("Total time", end-start)
