e663f29066b7f59ad7d54c000293c367e34e6e24
aarati-K <aarati.agc10@gmail.com>
2018-12-19 04:12:54 -0600

    Benchmark sample creator for Cifar10

--- benchmarks/cifar10/benchmark_sample_creator.py
+++ benchmarks/cifar10/benchmark_sample_creator.py
@@ -4,21 +4,28 @@ import time
 import matplotlib.pyplot as plt
 
 batch_size = 1
+rel_sample_size = 10000
 ds = Cifar10(input_data_folder="/home/aarati/datasets/cifar-10-batches-py", \
-    max_batches=2, batch_size=batch_size, rel_sample_size=10, \
+    max_batches=2, batch_size=batch_size, rel_sample_size=rel_sample_size, \
     max_samples=1, transform=transforms.ToTensor())
-start = time.time()
-ds.initialize()
-end = time.time()
-print(end-start)
+ds.count_num_points()
+ds.generate_IR()
+
+all_times = []
+for i in range(10):
+    start = time.time()
+    ds.initialize_samples()
+    end = time.time()
+    all_times.append(end-start)
+    s = ds.samples[0].get()
+print(all_times)
 
 # Sample creation time for sample size:
-# 1: 0.45
-# 10: 0.702
-# 50: 0.703
-# 100: 0.708
-# 1000: 0.715
-# 10000: 0.746
+# 1: [0.349, 0.306, 0.431, 0.303, 0.18, 0.69, 0.557, 0.681, 0.424, 0.300]
+# 10: [0.742, 0.685, 0.679, 0.676, 0.673, 0.676, 0.551, 0.673, 0.669, 0.670]
+# 100: [0.713, 0.672, 0.668, 0.671, 0.668, 0.680, 0.682, 0.675, 0.673, 0.669]
+# 1000: [0.738, 0.689, 0.704, 0.693, 0.684, 0.683, 0.678, 0.677, 0.700, 0.687]
+# 10000: [0.765, 0.727, 0.717, 0.740, 0.723, 0.774, 0.720, 0.868, 0.724, 0.771]
 
 # Plotting code
 # x = [1, 10, 50, 100, 1000, 10000]
